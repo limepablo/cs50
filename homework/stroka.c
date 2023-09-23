201fa86ad8a7d94601fb67b644c980da8d0a3fb4
privet <77719828+limepablo@users.noreply.github.com>
2023-09-23 12:33:39 +0000

    Sat, Sep 23, 2023, 2:33 PM +02:00

--- homework/stroka.c
+++ homework/stroka.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
-#include <string.h>
 
 int main() {
     char str[100];
 
     printf("Введіть строку: ");
-    fgets(str, sizeof(str), stdin);
-
-    int length = strlen(str);
+    fgets(str);
 
     printf("Строка в оберненому порядку: ");
-    for (int i = length - 1; i >= 0; i--) {
+
+    for (int i = strlen(str) - 1; i >= 0; i--) {
         printf("%c", str[i]);
     }
+
+    printf("\n");
 
     return 0;
 }
