e78684e363a202d8f83b5ff524c9e42350840f57
privet <77719828+limepablo@users.noreply.github.com>
2023-09-27 21:42:26 +0000

    Wed, Sep 27, 2023, 11:42 PM +02:00

--- problemset1/credit/credit.c
+++ problemset1/credit/credit.c
@@ -1,2 +1,68 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
+
+int main()
+{
+
+    long int card = get_long("Number: ");
+    int position = 0;
+    int sum = 0;
+    int firstnumber = 0;
+    int secondnumber = 0;
+
+    do
+    {
+        int lastnumber = card % 10;
+        secondnumber = firstnumber;
+        firstnumber = lastnumber;
+
+        position++;
+        if (position % 2 == 0)
+        {
+            lastnumber *= 2;
+
+            if (lastnumber < 10)
+            {
+                sum += lastnumber;
+            }
+            else
+            {
+                sum += lastnumber % 10;
+                sum += lastnumber / 10;
+            }
+        }
+        else
+        {
+            sum += lastnumber;
+        }
+
+        card /= 10;
+    }
+    while (card != 0);
+
+    if (sum % 10 == 0)
+    {
+        if (firstnumber == 4 && (position == 13 || position == 16))
+        {
+            printf("VISA\n");
+        }
+        else if (firstnumber == 5 && (secondnumber >= 1 && secondnumber <= 5) && position == 16)
+        {
+            printf("MASTERCARD\n");
+        }
+        else if (firstnumber == 3 && (secondnumber == 4 || secondnumber == 7) && position == 15)
+        {
+            printf("AMEX\n");
+        }
+        else
+        {
+            printf("INVALID\n");
+        }
+    }
+    else
+    {
+        printf("INVALID\n");
+    }
+
+    return 0;
+}
