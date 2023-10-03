47e2e16f95991a74fc334df9a02fe03f4c627e8c
privet <77719828+limepablo@users.noreply.github.com>
2023-10-03 18:44:48 +0000

    Tue, Oct 3, 2023, 8:44 PM +02:00

--- lecture/fibonacci.c
+++ lecture/fibonacci.c
@@ -1,2 +1,15 @@
 #include <stdio.h>
 
+int main()
+{
+    int con = 10;
+    int a = 0, b = 1, temp;
+
+    printf("Result:\n");
+
+    for(int i = 1; i <= con; i++)
+    {
+        printf("%i ", a);
+        
+    }
+}
