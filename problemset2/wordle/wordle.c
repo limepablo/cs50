dd6f273d3b00c4a0afe32f2b8c8603333c65802a
privet <77719828+limepablo@users.noreply.github.com>
2023-10-08 18:30:33 +0000

    Sun, Oct 8, 2023, 8:30 PM +02:00

--- problemset2/wordle/wordle.c
+++ problemset2/wordle/wordle.c
@@ -37,7 +37,7 @@ int main(int argc, string argv[])
 
     // ensure argv[1] is either 5, 6, 7, or 8 and store that value in wordsize instead
     // TODO #2
-    if (wordsize != 5 && wordsize != 6 && wordsize != 7 && wordsize !=8)
+    if (wordsize >= 5 && wordsize <= 8)
 
     // open correct file, each file has exactly LISTSIZE words
     char wl_filename[6];
