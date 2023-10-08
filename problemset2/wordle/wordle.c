1d993238699a9945e000a6d0328642c78ea5a215
privet <77719828+limepablo@users.noreply.github.com>
2023-10-08 19:28:20 +0000

    Sun, Oct 8, 2023, 9:28 PM +02:00

--- problemset2/wordle/wordle.c
+++ problemset2/wordle/wordle.c
@@ -1,6 +1,6 @@
 #include <cs50.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
@@ -13,10 +13,10 @@
 #define WRONG 0
 
 // ANSI color codes for boxed in letters
-#define GREEN   "\e[38;2;255;255;255;1m\e[48;2;106;170;100;1m"
-#define YELLOW  "\e[38;2;255;255;255;1m\e[48;2;201;180;88;1m"
-#define RED     "\e[38;2;255;255;255;1m\e[48;2;220;20;60;1m"
-#define RESET   "\e[0;39m"
+#define GREEN "\e[38;2;255;255;255;1m\e[48;2;106;170;100;1m"
+#define YELLOW "\e[38;2;255;255;255;1m\e[48;2;201;180;88;1m"
+#define RED "\e[38;2;255;255;255;1m\e[48;2;220;20;60;1m"
+#define RESET "\e[0;39m"
 
 // user-defined function prototypes
 string get_guess(int wordsize);
@@ -70,7 +70,7 @@ int main(int argc, string argv[])
     bool won = false;
 
     // print greeting, using ANSI color codes to demonstrate
-    printf(GREEN"This is WORDLE50"RESET"\n");
+    printf(GREEN "This is WORDLE50" RESET "\n");
     printf("You have %i tries to guess the %i-letter word I'm thinking of\n", guesses, wordsize);
 
     // main game loop, one iteration for each guess
@@ -140,7 +140,7 @@ int check_word(string guess, int wordsize, int status[], string choice)
 
     // compare guess to choice and score points as appropriate, storing points in status
     // TODO #5
-    for(int i = 0; i < wordsize; i++)
+    for (int i = 0; i < wordsize; i++)
     {
         if (guess[i] == choice[i])
         {
@@ -148,7 +148,7 @@ int check_word(string guess, int wordsize, int status[], string choice)
         }
         else
         {
-            for(int j = 0; j < wordsize; j++)
+            for (int j = 0; j < wordsize; j++)
             {
                 if (guess[i] == choice[j])
                 {
@@ -161,11 +161,11 @@ int check_word(string guess, int wordsize, int status[], string choice)
     }
     // HINTS
     // iterate over each letter of the guess
-        // iterate over each letter of the choice
-            // compare the current guess letter to the current choice letter
-                // if they're the same position in the word, score EXACT points (green) and break so you don't compare that letter further
-                // if it's in the word, but not the right spot, score CLOSE point (yellow)
-        // keep track of the total score by adding each individual letter's score from above
+    // iterate over each letter of the choice
+    // compare the current guess letter to the current choice letter
+    // if they're the same position in the word, score EXACT points (green) and break so you don't compare that letter further
+    // if it's in the word, but not the right spot, score CLOSE point (yellow)
+    // keep track of the total score by adding each individual letter's score from above
 
     return score;
 }
@@ -178,15 +178,15 @@ void print_word(string guess, int wordsize, int status[])
     {
         if (status[i] == EXACT)
         {
-            printf(GREEN"%c"RESET, guess[i]);
+            printf(GREEN "%c" RESET, guess[i]);
         }
         else if (status[i] == CLOSE)
         {
-            printf(YELLOW"%c"RESET, guess[i]);
+            printf(YELLOW "%c" RESET, guess[i]);
         }
         else
         {
-            printf(RED"%c"RESET, guess[i]);
+            printf(RED "%c" RESET, guess[i]);
         }
     }
 
