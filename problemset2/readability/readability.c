3c4511fd5c638b7bab3eb3cb5f68aef5884068f4
privet <77719828+limepablo@users.noreply.github.com>
2023-10-13 14:55:37 +0000

    Fri, Oct 13, 2023, 4:55 PM +02:00

--- problemset2/readability/readability.c
+++ problemset2/readability/readability.c
@@ -34,4 +34,17 @@ int main(void)
     float S = sent / words * 100
 
     int index = 0.0588 * L - 0.296 * S - 15.8
+
+    if (index >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else if (index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else
+    {
+        printf("Grade %i\n", index);
+    }
 }
