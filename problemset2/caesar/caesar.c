8719ca934d46edd3d9ca400208e884c10c73c655
privet <77719828+limepablo@users.noreply.github.com>
2023-10-13 20:11:40 +0000

    Fri, Oct 13, 2023, 10:11 PM +02:00

--- problemset2/caesar/caesar.c
+++ problemset2/caesar/caesar.c
@@ -1,7 +1,44 @@
+#include <string.h>
+#include <ctype.h>
 #include <cs50.h>
-#include <stdio.h>
 
 int main(int argc, string argv[])
 {
-    
+
+    if (argc == 2 && isdigit(*argv[1]))
+
+    {
+
+        int qwe = atoi(argv[1]);
+        string text = get_string("plaintext: ");
+        printf("ciphertext: ");
+
+        for (int i = 0, n = strlen(s) ; i < n; i++)
+        {
+            if (s[i] >= 'a' && s[i] <= 'z')
+            {
+                printf("%c", (((s[i] - 'a') + k) % 26) + 'a');
+            }
+            else if (s[i] >= 'A' && s[i] <= 'Z')
+            {
+                printf("%c", (((s[i] - 'A') + k) % 26) + 'A');
+            }
+
+            else
+            {
+                printf("%c", s[i]);
+            }
+        }
+
+        printf("\n");
+        return 0;
+    }
+
+    else
+    {
+        printf("Usage: ./caesar k\n");
+        return 1;
+
+    }
+
 }
