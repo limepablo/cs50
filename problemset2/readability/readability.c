0166e2422b0286ccb810705f8cd65ccb7307a82e
privet <77719828+limepablo@users.noreply.github.com>
2023-10-13 14:13:34 +0000

    Fri, Oct 13, 2023, 4:13 PM +02:00

--- problemset2/readability/readability.c
+++ problemset2/readability/readability.c
@@ -1,7 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
+    string text = get_string("Text: ");
 
+    int letters = 0
+    int words = 0
+    int sent = 0
+
+    for(int i = 0; i < strlen(text); i++)
+    {
+        if(text[i] > 65 && text[i] < 90)
+    }
 }
