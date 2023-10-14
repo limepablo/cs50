291c91c151e415d3b20c3056bbe394cc46af8b55
privet <77719828+limepablo@users.noreply.github.com>
2023-10-14 11:27:05 +0000

    Sat, Oct 14, 2023, 1:27 PM +02:00

---
+++ homework/NxN.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+
+    printf("Enter matrix size: ");
+    scanf("%d", &N);
+
+    int matrix[N][N];
+
+    printf("Enter matrix elements:\n");
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("matrix[%d][%d]: ", i, j);
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+
+    printf("Початкова матриця:\n");
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("%d\t", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+
+    for (int i = 0; i < N; i++) {
+        for (int j = i + 1; j < N; j++) {
+
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+
+    printf("Matrix with changed symmetrical elements:\n");
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("%d\t", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
