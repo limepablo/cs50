bbdab3d52297c9e6de948cb0d49523fd0b6494c4
privet <77719828+limepablo@users.noreply.github.com>
2023-10-15 20:21:47 +0000

    Sun, Oct 15, 2023, 10:21 PM +02:00

--- homework/NxN.c
+++ homework/NxN.c
@@ -18,7 +18,7 @@ int main()
     }
 
 
-    printf("Початкова матриця:\n");
+    printf("Cocha matrix:\n");
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             printf("%d\t", matrix[i][j]);
@@ -39,7 +39,7 @@ int main()
     printf("Matrix with changed symmetrical elements:\n");
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            printf("%d\t", matrix[i][j]);
+            printf("%d\n", matrix[i][j]);
         }
         printf("\n");
     }
