eb58e037985a73404067dcb74f97208be60e1a64
privet <77719828+limepablo@users.noreply.github.com>
2023-10-19 20:15:16 +0000

    Thu, Oct 19, 2023, 10:15 PM +02:00

--- homework/factorial.c
+++ homework/factorial.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 
 int factorial(int n) {
-    if (n >= 1) {
+    if (n <= 1) {
         return 1;
     } else {
         return n * factorial(n - 1);
     }
 }
 
+int main() {
+    int number = 6;
 
+    if (number < 0) {
+        printf("Factorial for this number does not exist.\n")
+    }
+}
