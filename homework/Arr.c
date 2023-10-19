183faa5d9a9324b97a3e423027448bdbddf075a1
privet <77719828+limepablo@users.noreply.github.com>
2023-10-19 13:17:06 +0000

    Thu, Oct 19, 2023, 3:17 PM +02:00

---
+++ homework/Arr.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int recursiveSum(int arr[], int n) {
+    
+    if (n == 0) {
+        return 0;
+    } else {
+
+        return arr[n - 1] + recursiveSum(arr, n - 1);
+    }
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int sum = recursiveSum(arr, n);
+
+    printf("The sum of the elements in the array is: %d\n", sum);
+
+    return 0;
+}
