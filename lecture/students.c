39f71a1f36a68effc394eca31b52e345a8655d5d
privet <77719828+limepablo@users.noreply.github.com>
2023-11-02 19:39:21 +0000

    Thu, Nov 2, 2023, 8:39 PM +01:00

--- lecture/students.c
+++ lecture/students.c
@@ -12,13 +12,13 @@ int main() {
     students array[count];
 
     for(int i = 0; i < count; i++) {
-        array[i].firstname = get_string("First name: \n");
-        array[i].secondname = get_string("Second name: \n");
-        array[i].year = get_int("Year: \n");
+        array[i].firstname = get_string("First name: ");
+        array[i].secondname = get_string("Second name: ");
+        array[i].year = get_int("Year: ");
     }
 
     for(int j = 0; j < count; j++) {
-        printf("%s %s, %i", array[j].firstname, array[j].secondname, array[j].year);
+        printf("%s %s, %i\n", array[j].firstname, array[j].secondname, array[j].year);
     }
 
 
