1c87719bfb21fb552ef5f88b2989a8d4e2f51cad
privet <77719828+limepablo@users.noreply.github.com>
2023-11-02 18:49:12 +0000

    Thu, Nov 2, 2023, 7:49 PM +01:00

--- lecture/students.c
+++ lecture/students.c
@@ -1,3 +1,15 @@
 #include <stdio.h>
 #include <cs50.h>
 
+typedef struct {
+    char firstname[10];
+    char secondname[10];
+    int year;
+} students;
+
+int main() {
+    int count = get_int("How many students:\n");
+
+    return 0;
+}
+
