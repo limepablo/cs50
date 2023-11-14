db822717cb2d4b4fd0c95169fd46e73176315cd7
privet <77719828+limepablo@users.noreply.github.com>
2023-11-14 15:46:15 +0000

    Tue, Nov 14, 2023, 4:46 PM +01:00

--- homework/calculator.c
+++ homework/calculator.c
@@ -2,25 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Функція для додавання
 double add(double a, double b) {
     return a + b;
 }
 
-// Функція для віднімання
 double subtract(double a, double b) {
     return a - b;
 }
 
-// Функція для множення
 double multiply(double a, double b) {
     return a * b;
 }
 
-// Функція для ділення
 double divide(double a, double b) {
     if (b == 0) {
-        printf("Помилка: ділення на нуль\n");
+        printf("You can't divide 0\n");
         exit(EXIT_FAILURE);
     }
     return a / b;
@@ -28,13 +24,13 @@ double divide(double a, double b) {
 
 int main(int argc, char *argv[]) {
     if (argc != 4) {
-        printf("Неправильна кількість аргументів. Використання: ./calculator число операція число\n");
+        printf("Invalid number of arguments. Usage: ./calculator number operation number\n");
         return EXIT_FAILURE;
     }
 
-    double num1 = atof(argv[1]); // перше число
-    char *operation = argv[2];   // операція
-    double num2 = atof(argv[3]); // друге число
+    double num1 = atof(argv[1]);
+    char *operation = argv[2];
+    double num2 = atof(argv[3]); 
 
     double result;
 
@@ -47,11 +43,11 @@ int main(int argc, char *argv[]) {
     } else if (strcmp(operation, "/") == 0) {
         result = divide(num1, num2);
     } else {
-        printf("Непідтримувана операція: %s\n", operation);
+        printf("You cannot do this %s\n", operation);
         return EXIT_FAILURE;
     }
 
-    printf("Результат: %.2f\n", result);
+    printf("Result: %.2f\n", result);
 
     return EXIT_SUCCESS;
 }
