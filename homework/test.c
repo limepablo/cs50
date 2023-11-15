09040c05123b7c631682eedab1a0f85d3f914839
privet <77719828+limepablo@users.noreply.github.com>
2023-11-15 15:25:02 +0000

    Wed, Nov 15, 2023, 4:25 PM +01:00

--- homework/test.c
+++ homework/test.c
@@ -6,10 +6,14 @@ int main() {
     int myArray[10];
 
     for (int i = 0; i < 10; i++) {
-        myArray[i] = 5;
+        myArray[i] = 10;
     }
 
-    for (int j = 0; )
+    for (int j = 0; j < 10; j++) {
+        printf("%d ", myArray[j]);
+    }
+
+    printf("\n");
 
     return 0;
 }
